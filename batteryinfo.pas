1b2716e4880c6ca37976f9bd95137c74dc230905
finalpatch <fengli@gmail.com>
2020-10-31 00:09:46 +1100

    initial checkin

---
+++ batteryinfo.pas
@@ -0,0 +1,175 @@
+unit BatteryInfo;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Windows, JwaBatClass;
+
+const
+  DIGCF_PRESENT = $00000002;
+  DIGCF_DEVICEINTERFACE = $00000010;
+  SetupApiModuleName = 'SetupApi.dll';
+
+type
+  HDEVINFO = Pointer;
+
+  PSPDeviceInterfaceData = ^TSPDeviceInterfaceData;
+
+  SP_DEVICE_INTERFACE_DATA = packed record
+    cbSize: DWORD;
+    InterfaceClassGuid: TGUID;
+    Flags: DWORD;
+    Reserved: ULONG_PTR;
+  end;
+  TSPDeviceInterfaceData = SP_DEVICE_INTERFACE_DATA;
+
+  PSPDevInfoData = ^TSPDevInfoData;
+
+  SP_DEVINFO_DATA = packed record
+    cbSize: DWORD;
+    ClassGuid: TGUID;
+    DevInst: DWORD; // DEVINST handle
+    Reserved: ULONG_PTR;
+  end;
+  TSPDevInfoData = SP_DEVINFO_DATA;
+
+  PSPDeviceInterfaceDetailDataA = ^TSPDeviceInterfaceDetailDataA;
+
+  SP_DEVICE_INTERFACE_DETAIL_DATA_A = packed record
+    cbSize: DWORD;
+    DevicePath: array [0..ANYSIZE_ARRAY - 1] of AnsiChar;
+  end;
+  TSPDeviceInterfaceDetailDataA = SP_DEVICE_INTERFACE_DETAIL_DATA_A;
+  TSPDeviceInterfaceDetailData = TSPDeviceInterfaceDetailDataA;
+  PSPDeviceInterfaceDetailData = PSPDeviceInterfaceDetailDataA;
+
+  TSetupDiGetClassDevs = function(ClassGuid: PGUID; const aEnumerator: PTSTR;
+    hwndParent: HWND; Flags: DWORD): HDEVINFO; stdcall;
+
+  TSetupDiDestroyDeviceInfoList = function(DeviceInfoSet: HDEVINFO): BOOL; stdcall;
+
+  TSetupDiEnumDeviceInterfaces = function(DeviceInfoSet: HDEVINFO;
+    DeviceInfoData: PSPDevInfoData; const InterfaceClassGuid: TGUID;
+    MemberIndex: DWORD; var DeviceInterfaceData: TSPDeviceInterfaceData): BOOL; stdcall;
+
+  TSetupDiGetDeviceInterfaceDetail = function(DeviceInfoSet: HDEVINFO;
+    DeviceInterfaceData: PSPDeviceInterfaceData;
+    DeviceInterfaceDetailData: PSPDeviceInterfaceDetailData;
+    DeviceInterfaceDetailDataSize: DWORD; var RequiredSize: DWORD;
+    Device: PSPDevInfoData): BOOL; stdcall;
+
+  { TBatteryInfo }
+
+  TBatteryInfo = class
+  private
+    hBat: HANDLE;
+    bws: TBatteryWaitStatus;
+  public
+    constructor Create;
+    destructor Destroy; override;
+    function GetBatteryStatus: TBatteryStatus;
+  end;
+
+implementation
+
+var
+  SetupApiLib: HINST;
+  SetupDiGetClassDevs: TSetupDiGetClassDevs;
+  SetupDiDestroyDeviceInfoList: TSetupDiDestroyDeviceInfoList;
+  SetupDiEnumDeviceInterfaces: TSetupDiEnumDeviceInterfaces;
+  SetupDiGetDeviceInterfaceDetail: TSetupDiGetDeviceInterfaceDetail;
+
+{ TBatteryInfo }
+
+constructor TBatteryInfo.Create;
+var
+  hDev: HDEVINFO;
+  iDev: integer;
+  did: TSPDeviceInterfaceData;
+  didd: PSPDeviceInterfaceDetailData;
+  res: BOOL;
+  dwSize: DWORD;
+  bqi: TBatteryQueryInformation;
+  dwWait: DWORD;
+  dwOut: DWORD;
+begin
+  hBat := INVALID_HANDLE_VALUE;
+
+  hDev := SetupDiGetClassDevs(@GUID_DEVICE_BATTERY, nil, 0, DIGCF_PRESENT or
+    DIGCF_DEVICEINTERFACE);
+  if hDev <> pointer(INVALID_HANDLE_VALUE) then
+  begin
+    for iDev := 0 to 10 do
+    begin
+      did.cbSize := SizeOf(did);
+      res := SetupDiEnumDeviceInterfaces(hDev, nil, GUID_DEVICE_BATTERY, iDev, did);
+      if not res then
+        break;
+      dwSize := 0;
+      SetupDiGetDeviceInterfaceDetail(hDev, @did, nil, 0, dwSize, nil);
+      if (dwSize <> 0) then
+      begin
+        didd := AllocMem(dwSize);
+        FillMemory(didd, dwSize, 0);
+        didd^.cbSize := 8;
+        res := SetupDiGetDeviceInterfaceDetail(hDev, @did, didd, dwSize, dwSize, nil);
+        if res then
+        begin
+          // got a battery, open it
+          hBat := CreateFile(didd^.DevicePath, GENERIC_READ or
+            GENERIC_WRITE, FILE_SHARE_READ or FILE_SHARE_WRITE, nil,
+            OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+          if (INVALID_HANDLE_VALUE <> hBat) then
+          begin
+            // Ask the battery for its tag
+            dwWait := 0;
+            FillMemory(@bqi, Sizeof(bqi), 0);
+            res := DeviceIoControl(hBat, IOCTL_BATTERY_QUERY_TAG,
+              @dwWait, sizeof(dwWait), @bqi.BatteryTag,
+              sizeof(bqi.BatteryTag), @dwOut, nil);
+            if res then
+            begin
+              FillMemory(@bws, Sizeof(bws), 0);
+              bws.BatteryTag := bqi.BatteryTag;
+            end;
+          end;
+        end;
+        FreeMem(didd);
+      end;
+    end;
+    SetupDiDestroyDeviceInfoList(hDev);
+  end;
+end;
+
+destructor TBatteryInfo.Destroy;
+begin
+  if (INVALID_HANDLE_VALUE <> hBat) then
+  begin
+    CloseHandle(hBat);
+  end;
+  inherited Destroy;
+end;
+
+function TBatteryInfo.GetBatteryStatus: TBatteryStatus;
+var
+  dwOut: DWORD;
+begin
+  DeviceIoControl(hBat, IOCTL_BATTERY_QUERY_STATUS, @bws, sizeof(bws), @Result,
+    sizeof(TBatteryStatus), @dwOut, nil);
+end;
+
+initialization
+  SetupApiLib := LoadLibrary(SetupApiModuleName);
+  pointer(SetupDiGetClassDevs) :=
+    GetProcedureAddress(SetupApiLib, 'SetupDiGetClassDevsA');
+  pointer(SetupDiDestroyDeviceInfoList) :=
+    GetProcAddress(SetupApiLib, 'SetupDiDestroyDeviceInfoList');
+  pointer(SetupDiEnumDeviceInterfaces) :=
+    GetProcAddress(SetupApiLib, 'SetupDiEnumDeviceInterfaces');
+  pointer(SetupDiGetDeviceInterfaceDetail) :=
+    GetProcAddress(SetupApiLib, 'SetupDiGetDeviceInterfaceDetailA');
+end.
+
+
---
+++ main.pas
@@ -0,0 +1,120 @@
+unit main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
+  Menus, JwaBatClass, BatteryInfo;
+
+type
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    ExitMenuItem: TMenuItem;
+    PopupMenu1: TPopupMenu;
+    Timer1: TTimer;
+    TrayIcon1: TTrayIcon;
+    bm: TBitmap;
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure ExitMenuItemClick(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+  private
+    bi: TBatteryInfo;
+    procedure UpdateTrayIcon;
+  public
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Timer1Timer(Sender: TObject);
+begin
+  UpdateTrayIcon;
+end;
+
+procedure TForm1.UpdateTrayIcon;
+var
+  bs: TBatteryStatus;
+  newString: string;
+  watts: single = 0.0;
+  fmt: string;
+begin
+  bs := bi.GetBatteryStatus;
+  if (bs.PowerState and BATTERY_CHARGING) <> 0 then begin
+    newString := '>>';
+  end else if (bs.PowerState and BATTERY_POWER_ON_LINE) <> 0 then begin
+    newString := 'AC';
+  end else begin
+    watts:=bs.Rate/1000.0;
+    if watts < 0 then
+      watts:=-watts;
+    if watts >= 10.0 then
+      fmt:='%-.1f'
+    else
+      fmt:='%-.2f';
+    newString := format(fmt,[watts]);
+  end;
+  if Label1.Caption<>newString then begin
+    if (bs.PowerState and BATTERY_POWER_ON_LINE) <> 0 then begin
+      bm.Canvas.Brush.Color:=clLime;
+      bm.Canvas.Font.Color:=clBlack;
+    end else if watts < 7.0 then begin
+      bm.Canvas.Brush.Color:=clBlue;
+      bm.Canvas.Font.Color:=clWhite;
+    end else if watts < 9.0 then begin
+      bm.Canvas.Brush.Color:=clTeal;
+      bm.Canvas.Font.Color:=clWhite;
+    end else if watts < 12.0 then begin
+      bm.Canvas.Brush.Color:=clYellow;
+      bm.Canvas.Font.Color:=clBlack;
+    end else if watts < 15.0 then begin
+      bm.Canvas.Brush.Color:=TColor($0080FF);
+      bm.Canvas.Font.Color:=clWhite;
+    end else begin
+      bm.Canvas.Brush.Color:=TColor($0000FF);
+      bm.Canvas.Font.Color:=clWhite;
+    end;
+    Label1.Caption:=newString;
+    bm.Canvas.FillRect(0, 0, bm.Width, bm.Height);
+    bm.Canvas.Font.Bold:=True;
+    bm.Canvas.TextRect(TRect.Create(0,0,bm.Width,bm.Height), 0, 0, newString);
+    TrayIcon1.Icon.Assign(bm);
+  end
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  bi := TBatteryInfo.Create;
+  bm := TBitmap.Create;
+  bm.SetSize(24,24);
+  bm.Canvas.Font.Name:='Segoe UI';
+  bm.Canvas.Font.Size:=6;
+  with bm.Canvas.TextStyle do begin
+    Alignment:=taCenter;
+    Layout := tlCenter;
+  end;
+  UpdateTrayIcon;
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+  bi.Free;
+  bm.Free;
+end;
+
+procedure TForm1.ExitMenuItemClick(Sender: TObject);
+begin
+  Form1.Close;
+end;
+
+end.
